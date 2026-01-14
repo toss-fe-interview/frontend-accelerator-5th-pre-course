ed0829b1daee4e75e2a56364e0f15ffe97cfd828
Deea <gohyuk528@naver.com>
2026-01-14 20:29:25 +0900

    feat: 상품 목록 필터기능 추가

--- src/features/savings-calculator/components/ProductList.tsx
+++ src/features/savings-calculator/components/ProductList.tsx
@@ -1,15 +1,20 @@
 import { Assets, colors, ListRow } from 'tosslib';
 import { formatNumber } from 'utils/format';
 import type { SavingsProduct } from '../api/api';
+import { useCalculatorParams } from '../hooks/useCalculatorParams';
 
 export default function ProductList({ products }: { products?: SavingsProduct[] }) {
-  if (!products) {
-    return null;
+  const { monthlyAmount, savingTerms } = useCalculatorParams();
+
+  const filteredProducts = products?.filter(matchesPaymentRange(monthlyAmount)).filter(matchesPeriod(savingTerms));
+
+  if (filteredProducts?.length === 0) {
+    return <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 상품이 없습니다." />} />;
   }
 
   return (
     <div>
-      {products?.map(product => (
+      {filteredProducts?.map(product => (
         <ListRow
           key={product.id}
           contents={
@@ -30,3 +35,11 @@ export default function ProductList({ products }: { products?: SavingsProduct[] 
     </div>
   );
 }
+
+const matchesPaymentRange = (amount: number | null) => (product: SavingsProduct) => {
+  return amount == null || (product.minMonthlyAmount <= amount && product.maxMonthlyAmount >= amount);
+};
+
+const matchesPeriod = (period: number | null) => (product: SavingsProduct) => {
+  return period == null || product.availableTerms === period;
+};
