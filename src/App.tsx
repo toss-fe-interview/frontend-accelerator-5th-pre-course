f1d0b737fd41066129466c16f7055742e1ef966a
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2026-01-14 20:25:08 +0900

    feat: queryClient 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
+import { QueryProvider } from 'libs/queryProvider';
 
 export function App() {
   return (
     <>
       <GlobalStyles />
       <GlobalPortal.Provider>
-        <Routes />
+        <QueryProvider>
+          <Routes />
+        </QueryProvider>
       </GlobalPortal.Provider>
     </>
   );
