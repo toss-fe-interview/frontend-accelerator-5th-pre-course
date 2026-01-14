37ed8c42ab7bbd52714e3e4bb6f5ed873d8e8a7d
jynam <njy1007@gmail.com>
2026-01-15 02:08:10 +0900

    fix: 임의의 값 삭제

--- src/useSavingsProductFilters.ts
+++ src/useSavingsProductFilters.ts
@@ -3,9 +3,8 @@ import { useState } from 'react';
 export type Term = 6 | 12 | 24;
 
 export default function useSavingsProductFilters() {
-  // TODO: 기본값 되돌리기
-  const [targetAmount, setTargetAmount] = useState<number>(1000000);
-  const [monthlyPayment, setMonthlyPayment] = useState<number>(100000);
+  const [targetAmount, setTargetAmount] = useState<number>(0);
+  const [monthlyPayment, setMonthlyPayment] = useState<number>(0);
   const [term, setTerm] = useState<Term>(12);
 
   const handleTargetAmountChange = (value: number) => {
