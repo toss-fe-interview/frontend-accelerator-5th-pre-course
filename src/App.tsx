27eb3848d0662eb95b91416d3f423c990e248795
bangdler <zbthz90@gmail.com>
2026-01-14 23:43:32 +0900

    chore: tanstack query 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <GlobalStyles />
       <GlobalPortal.Provider>
         <Routes />
       </GlobalPortal.Provider>
-    </>
+    </QueryClientProvider>
   );
 }
