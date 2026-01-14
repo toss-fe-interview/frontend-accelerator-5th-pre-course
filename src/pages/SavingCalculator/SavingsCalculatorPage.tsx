0295092467c582b0435cff86698cd1eafed28fc0
Youngjae Kim <gomgom0324@gmail.com>
2026-01-14 15:51:09 +0900

    feat: 탭전환을 위한 선택된 탭 상태 추가

--- src/pages/SavingCalculator/SavingsCalculatorPage.tsx
+++ src/pages/SavingCalculator/SavingsCalculatorPage.tsx
@@ -2,8 +2,12 @@ import { Border, NavigationBar, Spacing, Tab } from 'tosslib';
 import SavingCalculatorInput from './components/SavingCalculatorInput';
 import SavingItemList from './components/SavingItemList';
 import SavingResult from './components/SavingResult';
+import { useState } from 'react';
+
+type SelectedTab = 'products' | 'results';
 
 export function SavingsCalculatorPage() {
+  const [selectedTab, setSelectedTab] = useState<SelectedTab>('products');
   return (
     <>
       <NavigationBar title="적금 계산기" />
@@ -12,15 +16,16 @@ export function SavingsCalculatorPage() {
       <Border height={16} />
       <Spacing size={8} />
 
-      <Tab onChange={() => {}}>
-        <Tab.Item value="products" selected={true}>
+      <Tab onChange={value => setSelectedTab(value as SelectedTab)}>
+        <Tab.Item value="products" selected={selectedTab === 'products'}>
           적금 상품
         </Tab.Item>
-        <Tab.Item value="results" selected={false}>
+        <Tab.Item value="results" selected={selectedTab === 'results'}>
           계산 결과
         </Tab.Item>
       </Tab>
-      <SavingItemList />
+      {selectedTab === 'products' && <SavingItemList />}
+      {selectedTab === 'results' && <SavingResult />}
       <SavingResult />
     </>
   );
