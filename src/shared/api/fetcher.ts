d15e2419c6305cc67c807799ba6fc91e30410093
성정민 <hellojeongmin@gmail.com>
2026-01-15 01:50:27 +0900

    fix: 에러 인터페이스 수정

--- src/shared/api/fetcher.ts
+++ src/shared/api/fetcher.ts
@@ -1,14 +1,7 @@
-import { http, isHttpError } from 'tosslib';
+import { http } from 'tosslib';
 import type { SavingsProduct } from './type';
 
 export const getSavingsProducts = async () => {
-  try {
-    const response = await http.get<SavingsProduct[]>('/api/savings-products');
-    return response;
-  } catch (e) {
-    if (isHttpError(e)) {
-      console.error(e.message);
-      window.alert('적금 상품을 불러오는 데 실패했습니다.');
-    }
-  }
+  const response = await http.get<SavingsProduct[]>('/api/savings-products');
+  return response;
 };
