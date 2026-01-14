b02358706f115c460f3691a192fa43be7a67a275
성정민 <hellojeongmin@gmail.com>
2026-01-15 03:08:12 +0900

    fix: utils 폴더 삭제

--- src/pages/SavingsCalculator/ui/index.tsx
+++ src/pages/SavingsCalculator/ui/index.tsx
@@ -3,7 +3,6 @@ import { SavingsProduct } from 'shared/api/type';
 import { Assets, Border, ListHeader, ListRow, NavigationBar, Spacing, Tab } from 'tosslib';
 import { useGetSavingProducts } from '../hooks/useGetSavingProducts';
 import { useSavingForm } from '../hooks/useSavingForm';
-import { getFilterSavingProducts, getRecommendSavingProducts, toNumeric } from '../utils';
 import { Products } from './Products';
 import { SavingsProducts } from './Products/SavingsProducts';
 import { Results } from './Results';
@@ -100,3 +99,29 @@ export function SavingsCalculatorPage() {
     </>
   );
 }
+
+const getFilterSavingProducts = (
+  products: SavingsProduct[],
+  conditions: {
+    monthlyDeposit: number;
+    savingPeriod: number;
+  }
+) => {
+  const moreThanMinMonthly = (product: SavingsProduct) => product.minMonthlyAmount <= conditions.monthlyDeposit;
+  const lessThanMaxMonthly = (product: SavingsProduct) => product.maxMonthlyAmount >= conditions.monthlyDeposit;
+  const sameAvailableTerms = (product: SavingsProduct) => product.availableTerms === conditions.savingPeriod;
+
+  const filteredSavingProducts = products.filter(
+    product => moreThanMinMonthly(product) && lessThanMaxMonthly(product) && sameAvailableTerms(product)
+  );
+
+  return filteredSavingProducts;
+};
+
+const getRecommendSavingProducts = (products: SavingsProduct[], limit = 2) =>
+  products
+    .slice()
+    .sort((a, b) => b.annualRate - a.annualRate)
+    .slice(0, limit);
+
+const toNumeric = (value: string) => Number(value.replace(/,/g, ''));
