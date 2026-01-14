e8c4e4f28c04625b0f0e58966e6cceda53142179
YeongseoYoon <l112xpla@gmail.com>
2026-01-14 21:44:29 +0900

    feat: SavingsCalculateItem 컴포넌트 구현

---
+++ src/features/saving-products-calculate/components/Item.tsx
@@ -0,0 +1,22 @@
+import { ListRow, colors } from 'tosslib';
+
+type SavingsCalculateItemProps = {
+  label: string;
+  value: string;
+};
+
+export const SavingsCalculateItem = ({ label, value }: SavingsCalculateItemProps) => {
+  return (
+    <ListRow
+      contents={
+        <ListRow.Texts
+          type="2RowTypeA"
+          top={label}
+          topProps={{ color: colors.grey600 }}
+          bottom={value}
+          bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
+        />
+      }
+    />
+  );
+};
