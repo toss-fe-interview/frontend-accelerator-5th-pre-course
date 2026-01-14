a5810f05147a1d7af3f09491e6f8f6213ce80b91
Jinwon Jung <wlsdnjs16521@gmail.com>
2026-01-14 19:55:29 +0900

    feat: apply thousand math round

--- src/features/savings/utils/calculation/savings.ts
+++ src/features/savings/utils/calculation/savings.ts
@@ -5,17 +5,17 @@ export const calculateEstimatedEaringsAmount = (
 ) => {
   const result = monthlyPaymentAmount * savingsPeriod * (1 + annualRate * 0.5);
 
-  return Math.round(result);
+  return Math.round(result / 1000) * 1000;
 };
 
 export const calculateDifferenceWithTargetAmount = (targetAmount: number, estimatedEarginsAmount: number) => {
   const result = targetAmount - estimatedEarginsAmount;
 
-  return Math.round(result);
+  return Math.round(result / 1000) * 1000;
 };
 
 export const calculateRecommendedMonthlyPayment = (targetAmount: number, savingsPeriod: number, annualRate: number) => {
   const result = targetAmount / (savingsPeriod * (1 + annualRate * 0.5));
 
-  return Math.round(result);
+  return Math.round(result / 1000) * 1000;
 };
