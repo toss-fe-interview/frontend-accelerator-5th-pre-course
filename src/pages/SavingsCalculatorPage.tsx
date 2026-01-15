150e27cd64dbef9c34a5073318b5749718c22e3d
Jinwon Jung <wlsdnjs16521@gmail.com>
2026-01-16 02:06:33 +0900

    refactor: delete SavingsInputs

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -2,15 +2,15 @@ import { ErrorBoundary, Suspense } from '@suspensive/react';
 import ErrorFallback from 'components/ErrorFallback';
 import SuspenseFallback from 'components/SuspenseFallback';
 import CalculationResult from 'features/savings/components/CalculationResult';
-import SavingsInputs from 'features/savings/components/SavingsInputs';
 import SavingsProductList from 'features/savings/components/SavingsProductList';
 import { useSuspenseSavingsProducts } from 'features/savings/hooks/quries/useSuspenseSavingsProducts';
 import useFilteredSavingsProducts from 'features/savings/hooks/useFilteredSavingsProducts';
 import { SavingsValues } from 'features/savings/types/savingsValues';
 import { SavingsTabs } from 'features/savings/types/tabs';
+import { formatNumberWithComma } from 'features/savings/utils/format/number';
 import { parseNumberInput } from 'features/savings/utils/parse/number';
 import { ChangeEvent, useState } from 'react';
-import { Border, NavigationBar, Spacing, Tab } from 'tosslib';
+import { Border, NavigationBar, SelectBottomSheet, Spacing, Tab, TextField } from 'tosslib';
 
 export function SavingsCalculatorPage() {
   const [savingsValues, setSavingsValues] = useState<SavingsValues>({
@@ -28,31 +28,6 @@ export function SavingsCalculatorPage() {
     savingsValues.savingsPeriod
   );
 
-  const handleChagneTargetAmount = (e: ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseNumberInput(e.target.value);
-
-    setSavingsValues(prev => ({
-      ...prev,
-      targetAmount: newValue,
-    }));
-  };
-
-  const handleChagneMonthlyPaymentAmount = (e: ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseNumberInput(e.target.value);
-
-    setSavingsValues(prev => ({
-      ...prev,
-      monthlyPaymentAmount: newValue,
-    }));
-  };
-
-  const handleChangeSavingsPeriod = (newValue: number) => {
-    setSavingsValues(prev => ({
-      ...prev,
-      savingsPeriod: newValue,
-    }));
-  };
-
   const changeSelectedProduct = (newValue: string) => {
     setSelectedProductId(newValue);
   };
@@ -62,12 +37,52 @@ export function SavingsCalculatorPage() {
       <NavigationBar title="적금 계산기" />
       <Spacing size={16} />
 
-      <SavingsInputs
-        savingsValues={savingsValues}
-        onChangeTargetAmount={handleChagneTargetAmount}
-        onChangeMonthlyPaymentAmount={handleChagneMonthlyPaymentAmount}
-        onChangeSavingsPeriod={handleChangeSavingsPeriod}
+      <TextField
+        label="목표 금액"
+        placeholder="목표 금액을 입력하세요"
+        suffix="원"
+        value={formatNumberWithComma(savingsValues.targetAmount)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          const newValue = parseNumberInput(e.target.value);
+
+          setSavingsValues(prev => ({
+            ...prev,
+            targetAmount: newValue,
+          }));
+        }}
       />
+      <Spacing size={16} />
+      <TextField
+        label="월 납입액"
+        placeholder="희망 월 납입액을 입력하세요"
+        suffix="원"
+        value={formatNumberWithComma(savingsValues.monthlyPaymentAmount)}
+        onChange={(e: ChangeEvent<HTMLInputElement>) => {
+          const newValue = parseNumberInput(e.target.value);
+
+          setSavingsValues(prev => ({
+            ...prev,
+            monthlyPaymentAmount: newValue,
+          }));
+        }}
+      />
+      <Spacing size={16} />
+      <SelectBottomSheet
+        label="저축 기간"
+        title="저축 기간을 선택해주세요"
+        value={savingsValues.savingsPeriod}
+        onChange={(newValue: number) => {
+          setSavingsValues(prev => ({
+            ...prev,
+            savingsPeriod: newValue,
+          }));
+        }}
+      >
+        <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
+        <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
+        <SelectBottomSheet.Option value={18}>18개월</SelectBottomSheet.Option>
+        <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
+      </SelectBottomSheet>
 
       <Spacing size={24} />
       <Border height={16} />
