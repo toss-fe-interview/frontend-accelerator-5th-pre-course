03e42d1464630a7d70656442e47145b6a8899901
Jiwoo <jiwoo.link@gmail.com>
2026-01-15 21:11:36 +0900

    react-query 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 1,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export function App() {
   return (
     <>
-      <GlobalStyles />
-      <GlobalPortal.Provider>
-        <Routes />
-      </GlobalPortal.Provider>
+      <QueryClientProvider client={queryClient}>
+        <GlobalStyles />
+        <GlobalPortal.Provider>
+          <Routes />
+        </GlobalPortal.Provider>
+      </QueryClientProvider>
     </>
   );
 }
