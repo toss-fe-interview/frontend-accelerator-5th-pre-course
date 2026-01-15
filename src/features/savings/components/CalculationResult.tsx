c3409655f3a3c6971fbde44cea87f90ec7a02202
Jinwon Jung <wlsdnjs16521@gmail.com>
2026-01-16 02:52:55 +0900

    refactor: SavingsResult를 안에서 밖으로 꺼내기

--- src/features/savings/components/CalculationResult.tsx
+++ src/features/savings/components/CalculationResult.tsx
@@ -1,9 +1,14 @@
-import { Spacing, Border, ListHeader } from 'tosslib';
+import { Spacing, Border, ListHeader, ListRow } from 'tosslib';
 import SavingsProductList from './SavingsProductList';
 import { SavingsProduct } from '../schemas/savingsProduct';
 import useRecommendedSavingsProducts from '../hooks/useRecommendedSavingsProducts';
-import SavingsResult from './SavingsResult';
 import { SavingsValues } from '../types/savingsValues';
+import SavingsResult from './SavingsResult';
+import {
+  calculateEstimatedEaringsAmount,
+  calculateDifferenceWithTargetAmount,
+  calculateRecommendedMonthlyPayment,
+} from '../utils/calculation/savings';
 
 interface CalculationResultProps {
   savingsValues: SavingsValues;
@@ -18,15 +23,36 @@ export default function CalculationResult({
 }: CalculationResultProps) {
   const recommendedSavingsProducts = useRecommendedSavingsProducts(savingsProducts);
 
+  const selectedProduct = savingsProducts.find(product => product.id === selectedProductId);
+  const isProductNotSelected = !selectedProductId || !selectedProduct;
+
+  const { targetAmount, monthlyPaymentAmount, savingsPeriod } = savingsValues;
+
   return (
     <>
       <Spacing size={8} />
 
-      <SavingsResult
-        savingsValues={savingsValues}
-        savingsProducts={savingsProducts}
-        selectedProductId={selectedProductId}
-      />
+      {isProductNotSelected ? (
+        <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} />
+      ) : (
+        <>
+          <SavingsResult
+            label="예상 수익 금액"
+            value={calculateEstimatedEaringsAmount(monthlyPaymentAmount, savingsPeriod, selectedProduct.annualRate)}
+          />
+          <SavingsResult
+            label="목표 금액과의 차이"
+            value={calculateDifferenceWithTargetAmount(
+              targetAmount,
+              calculateEstimatedEaringsAmount(monthlyPaymentAmount, savingsPeriod, selectedProduct.annualRate)
+            )}
+          />
+          <SavingsResult
+            label="추천 월 납입 금액"
+            value={calculateRecommendedMonthlyPayment(targetAmount, savingsPeriod, selectedProduct.annualRate)}
+          />
+        </>
+      )}
 
       <Spacing size={8} />
       <Border height={16} />
--- src/features/savings/components/SavingsResult.tsx
+++ src/features/savings/components/SavingsResult.tsx
@@ -1,38 +1,23 @@
-import { ListRow } from 'tosslib';
-import {
-  calculateDifferenceWithTargetAmount,
-  calculateEstimatedEaringsAmount,
-  calculateRecommendedMonthlyPayment,
-} from '../utils/calculation/savings';
-import { SavingsValues } from '../types/savingsValues';
-import { SavingsProduct } from '../schemas/savingsProduct';
-import SavingsResultItem from './SavingsResultItem';
+import { colors, ListRow } from 'tosslib';
+import { formatNumberWithComma } from '../utils/format/number';
 
-interface SavingsResultProps {
-  savingsValues: SavingsValues;
-  savingsProducts: SavingsProduct[];
-  selectedProductId: string | null;
+interface SavingsResultItemProps {
+  label: string;
+  value: number;
 }
 
-export default function SavingsResult({ savingsValues, savingsProducts, selectedProductId }: SavingsResultProps) {
-  const selectedProduct = savingsProducts.find(product => product.id === selectedProductId);
-
-  if (!selectedProductId || !selectedProduct) {
-    return <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} />;
-  }
-
-  const { targetAmount, monthlyPaymentAmount, savingsPeriod } = savingsValues;
-  const { annualRate } = selectedProduct;
-
-  const estimatedEaringsAmount = calculateEstimatedEaringsAmount(monthlyPaymentAmount, savingsPeriod, annualRate);
-  const differnceWithTargetAmount = calculateDifferenceWithTargetAmount(targetAmount, estimatedEaringsAmount);
-  const recommendedMonthlyPayment = calculateRecommendedMonthlyPayment(targetAmount, savingsPeriod, annualRate);
-
+export default function SavingsResult({ label, value }: SavingsResultItemProps) {
   return (
-    <>
-      <SavingsResultItem label="예상 수익 금액" value={estimatedEaringsAmount} />
-      <SavingsResultItem label="목표 금액과의 차이" value={differnceWithTargetAmount} />
-      <SavingsResultItem label="추천 월 납입 금액" value={recommendedMonthlyPayment} />
-    </>
+    <ListRow
+      contents={
+        <ListRow.Texts
+          type="2RowTypeA"
+          top={label}
+          topProps={{ color: colors.grey600 }}
+          bottom={`${formatNumberWithComma(value)}원`}
+          bottomProps={{ fontWeight: 'bold', color: colors.blue600 }}
+        />
+      }
+    />
   );
 }
