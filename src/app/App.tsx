60edb3dd3e7def61d1723e07e36312a0adb4a441
yongmin.jeon <dydalsdl14142@gmail.com>
2026-01-15 18:32:37 +0900

    refactor: 쿼리 클라이언트 인스턴스 위치 변경

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,9 +2,9 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './Routes';
 
-export function App() {
-  const queryClient = new QueryClient();
+const queryClient = new QueryClient();
 
+export function App() {
   return (
     <>
       <GlobalStyles />
