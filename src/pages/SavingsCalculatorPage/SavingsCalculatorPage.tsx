2e53a230af59ec6005e0f23ce9e49a92780633bb
KIMSEUNGGYU <45627868+KIMSEUNGGYU@users.noreply.github.com>
2026-01-16 19:33:41 +0900

    refactor: 적금 입력기 입력항목 상위로 추출

--- src/pages/SavingsCalculatorPage/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage/SavingsCalculatorPage.tsx
@@ -2,9 +2,10 @@ import { ListRow, NavigationBar, Spacing } from 'tosslib';
 import { useForm } from 'react-hook-form';
 import { SavingsCalculatorContents } from './components/SavingsCalculatorContents';
 import { SavingsFilterForm } from './types/saving-filter-form';
-import { SavingFilterForm } from './components/SavingFilterForm';
 import { AsyncBoundary } from 'components/AsyncBoundary';
 import { SavingProductErrorFallback } from './components/fallback/SavingProductErrorFallback';
+import { CurrencyTextField } from './components/fields/CurrencyTextField';
+import { SelectTermField } from './components/fields/SelectTermField';
 
 export function SavingsCalculatorPage() {
   const form = useForm<SavingsFilterForm>({
@@ -22,7 +23,32 @@ export function SavingsCalculatorPage() {
       <NavigationBar title="적금 계산기" />
       <Spacing size={16} />
 
-      <SavingFilterForm control={form.control} />
+      {/* TODO: Flex 컴포넌트 */}
+      <div css={{ display: 'flex', flexDirection: 'column', gap: 16 }}>
+        <CurrencyTextField
+          label="목표 금액"
+          placeholder="목표 금액을 입력하세요"
+          value={targetAmount}
+          onChange={value => form.setValue('targetAmount', value)}
+        />
+        <CurrencyTextField
+          label="월 납입액"
+          placeholder="희망 월 납입액을 입력하세요"
+          value={monthlyPayment}
+          onChange={value => form.setValue('monthlyPayment', value)}
+        />
+        <SelectTermField
+          label="저축 기간"
+          title="저축 기간을 선택해주세요"
+          options={[
+            { value: 6, label: '6개월' },
+            { value: 12, label: '12개월' },
+            { value: 24, label: '24개월' },
+          ]}
+          value={term}
+          onSelect={value => form.setValue('term', value)}
+        />
+      </div>
       <Spacing size={24} />
 
       <AsyncBoundary
---
+++ src/pages/SavingsCalculatorPage/components/fields/CurrencyTextField.tsx
@@ -0,0 +1,25 @@
+import { ComponentProps } from 'react';
+import { TextField } from 'tosslib';
+import { formatCurrency } from 'utils/format';
+import { onlyNumbers } from 'utils/input';
+
+type TextFieldProps = ComponentProps<typeof TextField>;
+
+interface Props extends Omit<TextFieldProps, 'value' | 'onChange'> {
+  value: number | null;
+  onChange: (value: number | null) => void;
+}
+
+export function CurrencyTextField({ value, onChange, suffix = '원', ...props }: Props) {
+  return (
+    <TextField
+      suffix={suffix}
+      {...props}
+      value={value ? formatCurrency(value) : ''}
+      onChange={event => {
+        const numbers = onlyNumbers(event.target.value);
+        onChange(numbers ? Number(numbers) : null);
+      }}
+    />
+  );
+}
