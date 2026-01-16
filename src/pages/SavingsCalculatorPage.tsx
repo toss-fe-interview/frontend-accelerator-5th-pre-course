4fb62759a6fee397be114094d9688f73a9906e36
hmyo2853 <hmyo2853@gmail.com>
2026-01-16 21:44:44 +0900

    refactor: 적금 계산기 안 -> 밖

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -1,43 +1,65 @@
 import { useState } from 'react';
-import { NavigationBar, Spacing, Border } from 'tosslib';
-import { CalculatorFields } from '../components/CalculatorFields';
-import { ProductTabs } from '../components/ProductTabs';
-import { SavingsProduct } from '../components/ProductTabs/types';
-import { calculateExpectedProfit, calculateRecommendMonthlyPayment } from '../utils/math';
+import { NavigationBar, Spacing, Border, TextField, SelectBottomSheet } from 'tosslib';
 
 export function SavingsCalculatorPage() {
-  const [targetAmount, setTargetAmount] = useState('');
-  const [monthlyAmount, setMonthlyAmount] = useState('');
+  const [targetAmount, setTargetAmount] = useState(0);
+  const [monthlyAmount, setMonthlyAmount] = useState(0);
   const [savingTerms, setSavingTerms] = useState(12);
-  const [selectedSavingProduct, setSelectedSavingProduct] = useState<SavingsProduct | null>(null);
+  // const [selectedSavingProduct, setSelectedSavingProduct] = useState<SavingsProduct | null>(null);
 
-  const expectedProfit = calculateExpectedProfit(monthlyAmount, savingTerms, selectedSavingProduct?.annualRate ?? 0);
-  const diffAmount = Number(targetAmount ?? 0) - expectedProfit;
-  const recommendMonthlyPayment = calculateRecommendMonthlyPayment(
-    targetAmount,
-    savingTerms,
-    selectedSavingProduct?.annualRate ?? 0
-  );
+  // const expectedProfit = calculateExpectedProfit(monthlyAmount, savingTerms, selectedSavingProduct?.annualRate ?? 0);
+  // const diffAmount = Number(targetAmount ?? 0) - expectedProfit;
+  // const recommendMonthlyPayment = calculateRecommendMonthlyPayment(
+  //   targetAmount,
+  //   savingTerms,
+  //   selectedSavingProduct?.annualRate ?? 0
+  // );
 
   return (
     <>
       <NavigationBar title="적금 계산기" />
       <Spacing size={16} />
 
-      <CalculatorFields
-        targetAmount={targetAmount}
-        monthlyAmount={monthlyAmount}
-        savingTerms={savingTerms}
-        onTargetAmountChange={setTargetAmount}
-        onMonthlyAmountChange={setMonthlyAmount}
-        onSavingTermsChange={setSavingTerms}
-      />
+      <>
+        <TextField
+          label="목표 금액"
+          placeholder="목표 금액을 입력하세요"
+          suffix="원"
+          value={targetAmount.toLocaleString('ko-KR')}
+          onChange={e => {
+            const num = Number(e.target.value);
+            setTargetAmount(num);
+          }}
+        />
+        <Spacing size={16} />
+        <TextField
+          label="월 납입액"
+          placeholder="희망 월 납입액을 입력하세요"
+          suffix="원"
+          value={monthlyAmount.toLocaleString('ko-KR')}
+          onChange={e => {
+            const num = Number(e.target.value);
+            setMonthlyAmount(num);
+          }}
+        />
+        <Spacing size={16} />
+        <SelectBottomSheet
+          label="저축 기간"
+          title="저축 기간을 선택해주세요"
+          value={savingTerms}
+          onChange={setSavingTerms}
+        >
+          <SelectBottomSheet.Option value={6}>6개월</SelectBottomSheet.Option>
+          <SelectBottomSheet.Option value={12}>12개월</SelectBottomSheet.Option>
+          <SelectBottomSheet.Option value={24}>24개월</SelectBottomSheet.Option>
+        </SelectBottomSheet>
+      </>
 
       <Spacing size={24} />
       <Border height={16} />
       <Spacing size={8} />
 
-      <ProductTabs
+      {/* <ProductTabs
         targetAmount={targetAmount}
         monthlyAmount={monthlyAmount}
         savingTerms={savingTerms}
@@ -46,7 +68,7 @@ export function SavingsCalculatorPage() {
         expectedProfit={expectedProfit}
         diffAmount={diffAmount}
         recommendMonthlyPayment={recommendMonthlyPayment}
-      />
+      /> */}
     </>
   );
 }
--- src/utils/format.ts
+++
@@ -1,6 +0,0 @@
-export const formatKRAmount = (amount: number) => {
-  if (!amount) {
-    return '';
-  }
-  return amount.toLocaleString('ko-KR');
-};
