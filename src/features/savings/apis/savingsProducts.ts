0f845915a58f125f9273530afb73623981a839df
Jinwon Jung <wlsdnjs16521@gmail.com>
2026-01-17 01:58:09 +0900

    refactor: 불필요한 zod 의존성 삭제

--- src/features/savings/apis/savingsProducts.ts
+++ src/features/savings/apis/savingsProducts.ts
@@ -1,21 +1,12 @@
 import { http, isHttpError } from 'tosslib';
-import { SavingsProduct, savingsProductSchema } from '../schemas/savingsProduct';
-import z from 'zod';
+import { SavingsProduct } from '../types/savingsProduct';
 
 export const fetchSavingsProducts = async (): Promise<SavingsProduct[]> => {
   try {
-    const response = await http.get<SavingsProduct[]>('/api/savings-products');
-
-    const validated = z.array(savingsProductSchema).parse(response);
-
-    return validated;
+    return await http.get<SavingsProduct[]>('/api/savings-products');
   } catch (e) {
     if (isHttpError(e)) {
       throw new Error(`적금 상품 조회 실패: ${e.message}`);
-    }
-
-    if (e instanceof z.ZodError) {
-      throw new Error(`적금 상품 데이터 검증 실패: ${e.message}`);
     }
 
     throw e;
