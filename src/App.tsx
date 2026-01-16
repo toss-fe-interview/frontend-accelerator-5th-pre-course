1151960f9f5d9ad25a99da0647f790ebdbaa79ad
nave1437 <pianowell96@gmail.com>
2026-01-17 02:11:30 +0900

    refactor : queryClient 외부로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
-import { useState } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-export function App() {
-  const [queryClient] = useState(() => new QueryClient());
+const queryClient = new QueryClient();
 
+export function App() {
   return (
     <>
       <GlobalStyles />
