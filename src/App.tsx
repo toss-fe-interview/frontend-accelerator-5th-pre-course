5656dda963b8eebf68bdb612e6043d1e286f85d2
Youngjae Kim <gomgom0324@gmail.com>
2026-01-16 23:20:21 +0900

    fix: queryClient 인스턴스 주입 오류 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
 export function App() {
   return (
     <>
-      <QueryClientProvider client={new QueryClient()}>
+      <QueryClientProvider client={queryClient}>
         <GlobalStyles />
         <GlobalPortal.Provider>
           <Routes />
