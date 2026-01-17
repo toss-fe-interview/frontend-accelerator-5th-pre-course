50cf18471214611366b1a1cf6d45a1d530b7877e
성정민 <hellojeongmin@gmail.com>
2026-01-18 00:00:44 +0900

    fix; 아이콘 삭제

--- src/shared/icon/CheckCircleIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { Icon } from 'tosslib';
-
-export const CheckCircleIcon = () => {
-  return <Icon source="icon-check-circle-green" />;
-};
-
-// TODO: Add more icon components as needed
-// export const AlertIcon = () => { ... };
-// export const SuccessIcon = () => { ... };
