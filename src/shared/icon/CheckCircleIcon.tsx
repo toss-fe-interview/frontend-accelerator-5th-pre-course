7ad4daf0bfe63c03851bec4f502ddc7512e68e66
성정민 <hellojeongmin@gmail.com>
2026-01-17 23:01:25 +0900

    feat: 공용 아이콘 및 유틸리티 함수 라이브러리 추가

---
+++ src/shared/icon/CheckCircleIcon.tsx
@@ -0,0 +1,9 @@
+import { Icon } from 'tosslib';
+
+export const CheckCircleIcon = () => {
+  return <Icon source="icon-check-circle-green" />;
+};
+
+// TODO: Add more icon components as needed
+// export const AlertIcon = () => { ... };
+// export const SuccessIcon = () => { ... };
