57e8d091d92fb1aa4959a8f145b2b4a434610aa6
윤석규 <sg.yun@joongonara.co.kr>
2026-01-18 00:58:59 +0900

    refactor: TabScreen 적용 삭제
    - 레이아웃 로직을 수정해야 할 경우, 시점의 이동이 발생함
    - Tab 영역 하위 UI 가 추가되어야 하는 경우, props 가 추가되어 복잡성 증가
    - 레이아웃 컴포넌트 없이도, 위에서 아래로 레이아웃이 잘 보이기 때문에 분리 해제

--- src/pages/SavingsCalculatorPage.tsx
+++ src/pages/SavingsCalculatorPage.tsx
@@ -1,4 +1,4 @@
-import { Border, ListHeader, ListRow, NavigationBar, Spacing } from 'tosslib';
+import { Border, ListHeader, ListRow, NavigationBar, Spacing, Tab } from 'tosslib';
 import { useState } from 'react';
 import { useQuery } from '@tanstack/react-query';
 
@@ -7,11 +7,10 @@ import SavingsProduct from 'domains/savingsCalculator/components/SavingsProduct'
 import MonthlyAmountField from 'domains/savingsCalculator/components/form/MonthlyAmountField';
 import TargetAmountField from 'domains/savingsCalculator/components/form/TargetAmountField';
 import TermField from 'domains/savingsCalculator/components/form/TermField';
-import { getRecommendedProducts, rangeIn, sortByAnnualRateDesc } from 'domains/savingsCalculator/utils/filter';
+import { getRecommendedProducts, rangeIn } from 'domains/savingsCalculator/utils/filter';
 import { round1000, toMultiplier } from 'domains/savingsCalculator/utils/calculate';
 
 import SavingsQuery from 'shared/query/saving';
-import TabScreen from 'shared/components/layout/TabScreen';
 import IconCheckCircle from 'shared/components/Icon/IconCheckCircle';
 import { SavingsProductType } from 'shared/types/api/savings';
 
@@ -27,6 +26,7 @@ export function SavingsCalculatorPage() {
   const [term, setTerm] = useState<6 | 12 | 24>(12);
 
   const [selectedProduct, setSelectedProduct] = useState<SavingsProductType | null>(null);
+  const [activeTabId, setActiveTabId] = useState<'products' | 'results'>('products');
 
   const validateMatchedProduct = (product: SavingsProductType) => {
     const 저축_기간이_일치함 = product.availableTerms === term;
@@ -47,27 +47,32 @@ export function SavingsCalculatorPage() {
   const hasNoMatchedProducts = matchedProducts.length === 0;
 
   return (
-    <TabScreen
-      headers={<NavigationBar title="적금 계산기" />}
-      topContents={
+    <>
+      <NavigationBar title="적금 계산기" />
+      <Spacing size={16} />
+
+      <TargetAmountField label="목표 금액" value={targetAmount} onChange={value => setTargetAmount(value)} />
+      <Spacing size={16} />
+      <MonthlyAmountField label="월 납입액" value={monthlyPayment} onChange={value => setMonthlyPayment(value)} />
+      <Spacing size={16} />
+      <TermField label="저축 기간" value={term} onChange={value => setTerm(value)} />
+
+      <Spacing size={24} />
+      <Border height={16} />
+      <Spacing size={8} />
+
+      <Tab onChange={value => setActiveTabId(value as 'products' | 'results')}>
+        <Tab.Item value="products" selected={activeTabId === 'products'}>
+          적금 상품
+        </Tab.Item>
+        <Tab.Item value="results" selected={activeTabId === 'results'}>
+          계산 결과
+        </Tab.Item>
+      </Tab>
+
+      {activeTabId === 'products' && (
         <>
-          {/* 본질이 뭐지? 값을 "보여주기" + 값을 "변경하기" + "어떤 필드"인지 보여주기 */}
-          {/* 값을 보여줄 때, 이게 TextField인지, SelectBottomSheet 인지 알 필요가 있을까? => 이건 how 라고 생각된다 */}
-          {/* 이런 경우 본질을 더 잘 드러내기 위해 분리하면 어떨까? */}
-          <TargetAmountField label="목표 금액" value={targetAmount} onChange={value => setTargetAmount(value)} />
-          <Spacing size={16} />
-          <MonthlyAmountField label="월 납입액" value={monthlyPayment} onChange={value => setMonthlyPayment(value)} />
-          <Spacing size={16} />
-          <TermField label="저축 기간" value={term} onChange={value => setTerm(value)} />
-        </>
-      }
-      bottomTabs={[
-        {
-          id: 'products',
-          label: '적금 상품',
-          contents: (
-            <>
-              {/* 본질이 뭘까? 
+          {/* 본질이 뭘까? 
                - 목표금액, 월 납입액, 저축기간에 따라서 필터링 조건에 맞는 적금 상품을 보여준다. 
                - 사용자 입력이 없으면 ? 보여주지 않는다 or 모두 보여준다 
                 - 모두 보여준다
@@ -78,92 +83,85 @@ export function SavingsCalculatorPage() {
                    -> 사용자가 입력부터 시작하게 강제하여 자연스럽게 제품이 의도한 방향으로 흘러갈 수 있음 (o)
                    -> 보여줄 상품이 없다는 건 == 조건에 맞는 상품이 없다는 것 (hasNoMatchingProducts)
                    -> 보여줄 상품들 === 조건에 맞는 상품들 (matchingProducts)
-              */}
-              {hasNoMatchedProducts ? (
-                <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 상품이 없어요." />} />
-              ) : (
-                matchedProducts.map(product => {
-                  const isSelected = selectedProduct?.id === product.id;
+          */}
+          {hasNoMatchedProducts ? (
+            <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 상품이 없어요." />} />
+          ) : (
+            matchedProducts.map(product => {
+              const isSelected = selectedProduct?.id === product.id;
 
-                  return (
-                    <ListRow
-                      key={product.id}
-                      contents={<SavingsProduct product={product} />}
-                      right={isSelected ? <IconCheckCircle /> : undefined}
-                      onClick={() => setSelectedProduct(product)}
-                    />
-                  );
-                })
-              )}
-            </>
-          ),
-        },
-        {
-          id: 'results',
-          label: '계산 결과',
-          contents: (
+              return (
+                <ListRow
+                  key={product.id}
+                  contents={<SavingsProduct product={product} />}
+                  right={isSelected ? <IconCheckCircle /> : undefined}
+                  onClick={() => setSelectedProduct(product)}
+                />
+              );
+            })
+          )}
+        </>
+      )}
+
+      {activeTabId === 'results' && (
+        <>
+          <Spacing size={8} />
+
+          {!selectedProduct ? (
+            <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} />
+          ) : (
             <>
-              <Spacing size={8} />
-
-              {!selectedProduct ? (
-                <ListRow contents={<ListRow.Texts type="1RowTypeA" top="상품을 선택해주세요." />} />
-              ) : (
-                <>
-                  <ListRow
-                    contents={
-                      <CalculationResult
-                        label="예상 수익 금액"
-                        value={monthlyPayment * term * toMultiplier(selectedProduct.annualRate)}
-                      />
-                    }
+              <ListRow
+                contents={
+                  <CalculationResult
+                    label="예상 수익 금액"
+                    value={monthlyPayment * term * toMultiplier(selectedProduct.annualRate)}
                   />
-                  <ListRow
-                    contents={
-                      <CalculationResult
-                        label="목표 금액과의 차이"
-                        value={targetAmount - monthlyPayment * term * toMultiplier(selectedProduct.annualRate)}
-                      />
-                    }
-                  />
-                  <ListRow
-                    contents={
-                      <CalculationResult
-                        label="추천 월 납입 금액"
-                        value={round1000(targetAmount / (term * toMultiplier(selectedProduct.annualRate)))}
-                      />
-                    }
-                  />
-                </>
-              )}
-
-              <Spacing size={8} />
-              <Border height={16} />
-              <Spacing size={8} />
-
-              <ListHeader
-                title={<ListHeader.TitleParagraph fontWeight="bold">추천 상품 목록</ListHeader.TitleParagraph>}
+                }
               />
-              <Spacing size={12} />
-
-              {hasNoMatchedProducts ? (
-                <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 상품이 없어요." />} />
-              ) : (
-                getRecommendedProducts(matchedProducts).map(product => {
-                  const isSelected = selectedProduct?.id === product.id;
-
-                  return (
-                    <ListRow
-                      key={product.id}
-                      contents={<SavingsProduct product={product} />}
-                      right={isSelected ? <IconCheckCircle /> : undefined}
-                    />
-                  );
-                })
-              )}
+              <ListRow
+                contents={
+                  <CalculationResult
+                    label="목표 금액과의 차이"
+                    value={targetAmount - monthlyPayment * term * toMultiplier(selectedProduct.annualRate)}
+                  />
+                }
+              />
+              <ListRow
+                contents={
+                  <CalculationResult
+                    label="추천 월 납입 금액"
+                    value={round1000(targetAmount / (term * toMultiplier(selectedProduct.annualRate)))}
+                  />
+                }
+              />
             </>
-          ),
-        },
-      ]}
-    />
+          )}
+
+          <Spacing size={8} />
+          <Border height={16} />
+          <Spacing size={8} />
+
+          <ListHeader title={<ListHeader.TitleParagraph fontWeight="bold">추천 상품 목록</ListHeader.TitleParagraph>} />
+          <Spacing size={12} />
+
+          {hasNoMatchedProducts ? (
+            <ListRow contents={<ListRow.Texts type="1RowTypeA" top="조건에 맞는 상품이 없어요." />} />
+          ) : (
+            getRecommendedProducts(matchedProducts).map(product => {
+              const isSelected = selectedProduct?.id === product.id;
+
+              return (
+                <ListRow
+                  key={product.id}
+                  contents={<SavingsProduct product={product} />}
+                  right={isSelected ? <IconCheckCircle /> : undefined}
+                />
+              );
+            })
+          )}
+        </>
+      )}
+    </>
   );
 }
