81b18a0d8161b980ddb2903d0882be3010f3bb5d
yongmin.jeon <dydalsdl14142@gmail.com>
2026-01-17 12:06:46 +0900

    refactor: 불필요 컴포넌트 제거

--- src/entities/savings/ui/index.ts
+++ src/entities/savings/ui/index.ts
@@ -1,6 +1,5 @@
 import SavingsProduct from './SavingsProductInfo';
 import CalculationResult from './CalculationResult';
-import Placeholder from './Placeholder';
 import Message from './Message';
 
 export { SavingsProduct, CalculationResult, Message };
