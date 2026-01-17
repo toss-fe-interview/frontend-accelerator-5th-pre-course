814e153a8a5f5ad80d834f2a008f7140976c66ca
hmyo2853 <hmyo2853@gmail.com>
2026-01-17 17:33:06 +0900

    remove: 타입 제거

--- src/components/ProductTabs/api.ts
+++ src/components/ProductTabs/api.ts
@@ -1,6 +1,6 @@
 import { http } from 'tosslib';
 import { SavingsProduct } from './types';
 
-export const fetchSavingsProducts = async (): Promise<SavingsProduct[]> => {
+export const fetchSavingsProducts = async () => {
   return await http.get<SavingsProduct[]>('/api/savings-products');
 };
