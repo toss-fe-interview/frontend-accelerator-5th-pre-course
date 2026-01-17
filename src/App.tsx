76c27514ffe2fd1e02029fa9892f8b7c290c2922
lydiacho <sheecho0804@ewhain.net>
2026-01-17 18:26:26 +0900

    fix: queryClient 생성 부 수정

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import { GlobalPortal, GlobalStyles } from 'tosslib';
 import { Routes } from './pages/Routes';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
+
 export function App() {
-  const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
       <GlobalStyles />
