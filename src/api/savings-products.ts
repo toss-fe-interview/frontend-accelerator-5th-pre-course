732a411e0c7be35e2391f2a12694e16db68259dc
Salvation_sub <kyman19940214@gmail.com>
2026-01-18 16:15:14 +0900

    refactor: getSavingsProducts 풀어쓰기

--- src/api/savings-products.ts
+++ src/api/savings-products.ts
@@ -2,12 +2,10 @@ import { queryOptions } from '@tanstack/react-query';
 import { http } from 'tosslib';
 import { SavingsProduct } from 'types/savings';
 
-export const savingsProductApis = {
-  getSavingsProducts: async () => await http.get<SavingsProduct[]>('/api/savings-products'),
-};
+export const getSavingsProducts = async () => await http.get<SavingsProduct[]>('/api/savings-products');
 
 export const getSavingsProductsQueryOptions = () =>
   queryOptions({
     queryKey: ['savingsProductsList'],
-    queryFn: savingsProductApis.getSavingsProducts,
+    queryFn: getSavingsProducts,
   });
