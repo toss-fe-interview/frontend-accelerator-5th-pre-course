2dc60f8733f9d6f3cd06f7cd12dea11655880973
Jinwon Jung <wlsdnjs16521@gmail.com>
2026-01-18 17:09:02 +0900

    feat: 에러바운더리에서 재시도 기능 추가

--- src/components/ErrorFallback.tsx
+++ src/components/ErrorFallback.tsx
@@ -1,8 +1,12 @@
-export default function ErrorFallback({ error }: { error: Error }) {
+import { ErrorBoundaryFallbackProps } from '@suspensive/react';
+
+export default function ErrorFallback({ error, reset }: ErrorBoundaryFallbackProps) {
   return (
     <div>
       <p>문제가 발생했습니다.</p>
       <p>{error.message}</p>
+
+      <button onClick={reset}>재시도</button>
     </div>
   );
 }
