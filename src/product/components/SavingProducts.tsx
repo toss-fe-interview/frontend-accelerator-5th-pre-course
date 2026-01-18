8f84d06c34fa04ea023a245f8a6ba75c029a3332
nave1437 <pianowell96@gmail.com>
2026-01-18 15:55:48 +0900

    refactor : 리스트 > 아이템으로 추상화 단위 수정

--- src/product/components/SavingProducts.tsx
+++ src/product/components/SavingProducts.tsx
@@ -1,43 +1,21 @@
-import { SavingProduct } from 'product/type/internal';
-import { Assets, colors, ListRow } from 'tosslib';
+import type { SavingProduct as SavingProductType } from 'product/type/internal';
+import SavingProduct from './SavingProduct';
 
-type Props =
-  | {
-      type: 'recommended';
-      data: SavingProduct[];
-      selectedProduct: SavingProduct | null;
-      selectProduct?: never;
-    }
-  | {
-      type: 'select';
-      data: SavingProduct[];
-      selectedProduct: SavingProduct | null;
-      selectProduct: (product: SavingProduct) => void;
-    };
+type Props = {
+  data: SavingProductType[];
+  selectedProduct: SavingProductType | null;
+  selectProduct: (product: SavingProductType) => void;
+};
 
-const SavingProducts = ({ type, data, selectedProduct, selectProduct }: Props) => {
+const SavingProducts = ({ data, selectedProduct, selectProduct }: Props) => {
   return data.map(product => (
-    <ListRow
+    <SavingProduct
       key={product.id}
-      contents={
-        <ListRow.Texts
-          type="3RowTypeA"
-          top={product.name}
-          topProps={{ fontSize: 16, fontWeight: 'bold', color: colors.grey900 }}
-          middle={`연 이자율: ${product.annualRate}%`}
-          middleProps={{ fontSize: 14, color: colors.blue600, fontWeight: 'medium' }}
-          bottom={`${product.minMonthlyAmount.toLocaleString()}원 ~ ${product.maxMonthlyAmount.toLocaleString()}원 | ${product.availableTerms}개월`}
-          bottomProps={{ fontSize: 13, color: colors.grey600 }}
-        />
-      }
-      right={selectedProduct?.id === product.id ? <Assets.Icon name="icon-check-circle-green" /> : null}
+      product={product}
       onClick={() => {
-        if (type === 'recommended') {
-          return;
-        }
-
         selectProduct(product);
       }}
+      selected={selectedProduct?.id === product.id}
     />
   ));
 };
